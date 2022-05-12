eb77009966246d8fe5a2186f18392765deae8d34
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-05-12 23:48:41 +1000

    commit

--- Main.cpp
+++ Main.cpp
@@ -8,18 +8,18 @@ int main()
     std::ofstream file("test.txt");
     file.close();
 
-    SortTester::SetMinCount(1e4);
-    SortTester::SetMaxCount(1e4);
-
-    for (int i = 0; i < 10; i++)
-    {
-        SortTester::SetSrand(i);
-        SortTester::FullTest(Sorting::bubble_flag, "bubble");
-    }
+    SortTester::SetMinCount(1e1);
+    SortTester::SetMaxCount(1e5);
 
     // for (int i = 0; i < 10; i++)
     // {
     //     SortTester::SetSrand(i);
-    //     SortTester::FullTest(Sorting::merge_sort, "merge");
+    //     SortTester::FullTest(Sorting::bubble_flag, "bubble");
     // }
+
+    for (int i = 0; i < 10; i++)
+    {
+        SortTester::SetSrand(i);
+        SortTester::FullTest(Sorting::merge_sort, "merge");
+    }
 }
